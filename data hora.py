c55482cce7fd9ac07a3fee723ebf1f01316de8e0
ViniciusCavalcante2000 <49639867+ViniciusCavalcante2000@users.noreply.github.com>
2021-06-17 19:46:22 -0300

    Add files via upload

--- data hora.py
+++ data hora.py
@@ -1,9 +1,11 @@
+# Mostrador de Data e Hora renováveis a cada 60 segundos
+
 from _datetime import datetime
 import time
 
 datetime.now().strftime('%d-%m-%Y %H:%M:%S')
 print(datetime.now().strftime('%d-%m-%Y %H:%M:%S'))
 while True:
-    if datetime.now().strftime('%d-%m-%Y %H:%M:%S')==datetime.now().strftime('%d-%m-%Y %H:%M:00'):
+    if datetime.now().strftime('%d-%m-%Y %H:%M:%S') == datetime.now().strftime('%d-%m-%Y %H:%M:00'):
         print(datetime.now().strftime('%d-%m-%Y %H:%M:%S'))
         time.sleep(60)
